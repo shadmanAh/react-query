fa6b835c7011b5c694c0959ca1ce62992be3b670
shadmanAh <shadman061@gmail.com>
2023-06-12 17:22:21 +0330

    first commit

---
+++ starter/src/Items.jsx
@@ -0,0 +1,23 @@
+import SingleItem from './SingleItem'
+import { useFetchTasks } from './reactQueryCustomHooks'
+
+const Items = () => {
+  const { isLoading, isError, data, error } = useFetchTasks()
+  if (isLoading) {
+    return <p style={{ marginTop: '1rem' }}>Loading...</p>
+  }
+  // if (isError) {
+  //   return <p style={{ marginTop: '1rem' }}>There was an error...</p>
+  // }
+  if (error) {
+    return <p style={{ marginTop: '1rem' }}>{error.response.data}</p>
+  }
+  return (
+    <div className="items">
+      {data.taskList.map((item) => {
+        return <SingleItem key={item.id} item={item} />
+      })}
+    </div>
+  )
+}
+export default Items
